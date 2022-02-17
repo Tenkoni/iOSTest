b3dd5917eadaab74723530944ec6438d00e04986
Luis <caliminion@gmail.com>
2022-02-16 23:22:32 -0600

    Adds the new cell class for name, with adjustments for its proper display, character filtering and keyboard management,

--- iOSTest/Controller/FillingViewController.swift
+++ iOSTest/Controller/FillingViewController.swift
@@ -10,36 +10,53 @@ import UIKit
 class FillingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var optionsList: [CellType]!
+    //keeping track of the currently selected cell
     var curretCellIndex: IndexPath!
     
     let tableView: UITableView = {
         let tableView = UITableView()
+        //defining the cells our table is going to have
         tableView.register(CameraCell.self, forCellReuseIdentifier: CellType.Camera.rawValue)
         tableView.register(PictureCell.self, forCellReuseIdentifier: CellType.Photo.rawValue)
+        tableView.register(NameCell.self, forCellReuseIdentifier: CellType.Name.rawValue)
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Add your information"
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.keyboardDismissMode = .onDrag
         view.addSubview(tableView)
-        // Do any additional setup after loading the view.
+
     }
     
     override func viewWillLayoutSubviews() {
+        //currently the tableview takes all the view area, will change
+        //later to only use the safe guide layout
         tableView.frame = view.bounds
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //we display an specific cell class according to the parameters supplied
+        //on the first view's table. Using the enumerator to identify them
         switch optionsList[indexPath.row] {
         case CellType.Camera:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: CellType.Camera.rawValue) as? CameraCell else { fatalError("Unable to dequeue cell.") }
             return cell
         case CellType.Photo:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: CellType.Photo.rawValue) as? PictureCell else { fatalError("Unable to dequeue cell.") }
+            //fetching an image (async), as required
             fetchImageFrom(url: "https://http2.mlstatic.com/vegeta-tamano-real-para-armar-en-papercraft-D_NQNP_892880-MLA26232224460_102017-F.jpg", for: cell)
+            return cell
+        case CellType.Name:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellType.Name.rawValue) as? NameCell else { fatalError("Unable to dequeue cell.") }
             return cell
         default:
             return UITableViewCell()
@@ -94,6 +111,23 @@ class FillingViewController: UIViewController, UITableViewDataSource, UITableVie
                 }
             }
         }
+    }
+    
+    @objc func adjustKeyboard(notification: Notification) {
+        //getting the keyboard object, included in the notification
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey]  as? NSValue else { return }
+        //getting the frame of the keyboard to locate it on screen
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, to: view.window)
+        //depending the notification type we will adjust the table accordingly
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            tableView.contentInset = .zero
+        } else {
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        tableView.scrollIndicatorInsets = tableView.contentInset
+        
     }
     
 }
---
+++ iOSTest/View/NameCell.swift
@@ -0,0 +1,59 @@
+//
+//  NameCell.swift
+//  iOSTest
+//
+//  Created by Lui on 16/02/22.
+//
+
+import UIKit
+
+class NameCell: UITableViewCell, UITextFieldDelegate {
+
+    let cellType = CellType.Name
+    
+    private let textBox: UITextField = {
+        let textBox = UITextField()
+        textBox.placeholder = "Nombre Completo"
+        return textBox
+        
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .blue
+        textBox.delegate = self
+        contentView.addSubview(textBox)
+    }
+    
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textBox.frame =  CGRect(x: 20, y: 0, width: contentView.frame.width - 40, height: contentView.frame.height)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        //first off the character filter
+        //get the character set of the characters in the new text
+        let characterSet = CharacterSet(charactersIn: string)
+        //next we declare the character space of unicode that would include only letters and whitespaces, what we expect a name to contain
+        let unicodeLetters = CharacterSet.letters
+        let unicodeWhiteSpace = CharacterSet.whitespaces
+        
+        //now the lenght filter
+        let currentText = textField.text ?? ""
+        //reading the range of characters that are going to be changed
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        //now we replace the caracters in the range to be edited
+        let newText = currentText.replacingCharacters(in: stringRange, with: string)
+        
+        //here we verify if the new characters are in the unicode letter and whitespace sets, if its in either of those and the length of the updated text is under 35 characters, we allow the editing
+        return (unicodeLetters.isSuperset(of: characterSet) || unicodeWhiteSpace.isSuperset(of: characterSet)) && newText.count <= 35
+    }
+    
+}
